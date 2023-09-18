d34a28c04fdd0ca0f824436bbf76738f2ce48241
unkletayo <akinsanyaadetayo@gmail.com>
2023-09-18 12:41:54 +0100

    handle file parsing

--- main.go
+++ main.go
@@ -1,15 +1,27 @@
 package main
 
 import (
+	"html/template"
+	"log"
 	"net/http"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
+var tpl = template.Must(template.ParseFiles("index.html"))
+
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("<h1> Hello Worls</h1>"))
+	tpl.Execute(w, nil)
 }
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Println("Error loading .env file")
+	}
+
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "3000"
